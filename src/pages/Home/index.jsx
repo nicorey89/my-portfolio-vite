3a4619eaf4518c02477cd409037c96d958ca6ad2
nicorey89 <nicolasthian8907@gmail.com>
2023-07-07 12:00:31 -0300

    2

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -4,6 +4,7 @@ export default function Home () {
     return (
         <main className={styles.Home} >
             <h2 className={styles.titular}>Bienvenidos a mi Portfolio!</h2>
+            <h2 className={styles.titular}>Estoy trabajando en esto!</h2>
             
         </main>
     )
